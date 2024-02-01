b1201abe977ed54a7805bef8acfa0d15ceb7f5f3
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-02-01 13:03:11 -0500

    v1.0.0

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ locals {
 
   # Read file content to get all lines.
   raw_lines = [
-    for line in split("\n", file("./policies/glocal/allowed-providers.sentinel")) :
+    for line in split("\n", file("./policies/global/allowed-providers.sentinel")) :
     line if startswidth(line, "#") || startswidth(line, "")
   ]
 
