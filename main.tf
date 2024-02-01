c06c4c26ec07c2086718a3ed1eb81b2313cd462e
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-02-01 13:17:08 -0500

    v1.0.0

--- main.tf
+++ main.tf
@@ -23,10 +23,16 @@ locals {
   files = fileset(local.policies_folder, "*/*.sentinel")
 }
 
-resource "tfe_policy" "this" {
+module "description" {
   for_each = local.files
-  name     = element(split(".", element(split("/", each.key), 1)), 2)
-  # description  = ""
+  source   = "./modules/get_description"
+  path     = each.value
+}
+
+resource "tfe_policy" "this" {
+  for_each     = local.files
+  name         = element(split(".", element(split("/", each.key), 1)), 2)
+  description  = module.description[each.value].description
   organization = local.organization_name
   kind         = "sentinel"
   policy       = file("${local.policies_folder}/${each.key}")
@@ -44,45 +50,4 @@ resource "tfe_policy_set" "global" {
   global       = true
   kind         = "sentinel"
   policy_ids   = [for value in tfe_policy.this : value.id]
-}
-
-locals {
-
-  # Read file content to get all lines.
-  raw_lines = [
-    for line in split("\n", file("./policies/global/allowed-providers.sentinel")) :
-    line if startswith(line, "#") || startswith(line, "")
-  ]
-
-  # Get the index of the first blank line.
-  # The description should be solid text followed by a blank line. 
-  index = index(local.raw_lines, "")
-
-  # Extract description line from the entire file.
-  # The description should starts on the first line of the file and finish with the blank line identify in the index.
-  description_lines = slice(local.raw_lines, 0, local.index)
-  
-  # A commented line must begin with the "#" symbol.
-  # Because we don't want that symbol in the description, it is removed from each line.
-  uncommented_lines = [
-    for line in local.description_lines : 
-    trimspace(trimprefix(line, "#"))
-  ]
-
-  # The description can be written on multiple line.
-  # Because of that, we need to join every line into a single string for the description.
-  description = join( " ", local.uncommented_lines)
-}
-
-output "raw_lines" {
-  value = local.raw_lines
-}
-
-output "index" {
-  value = local.index
-}
-
-output "description" {
-  description = "The description of the Sentinel policy file."
-  value       = local.description
 }
