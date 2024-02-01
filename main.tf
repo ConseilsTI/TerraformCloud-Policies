c6160a06085e02631834ab33b58c706dd430c324
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-02-01 13:00:40 -0500

    v1.0.0

--- main.tf
+++ main.tf
@@ -46,33 +46,43 @@ resource "tfe_policy_set" "global" {
   policy_ids   = [for value in tfe_policy.this : value.id]
 }
 
-
 locals {
+
+  # Read file content to get all lines.
   raw_lines = [
-    for line in split("\n", file("./policies/global/allowed-providers.sentinel")) :
-    line
+    for line in split("\n", file("./policies/glocal/allowed-providers.sentinel")) :
+    line if startswidth(line, "#") || startswidth(line, "")
   ]
-  first_blank_line = index(local.raw_lines, "")
-  description_lines = slice(local.raw_lines, 0, local.first_blank_line)
-  updated_description_lines = [
+
+  # Get the index of the first blank line.
+  # The description should be solid text followed by a blank line. 
+  index = index(local.raw_lines, "")
+
+  # Extract description line from the entire file.
+  # The description should starts on the first line of the file and finish with the blank line identify in the index.
+  description_lines = slice(local.raw_lines, 0, local.index)
+  
+  # A commented line must begin with the "#" symbol.
+  # Because we don't want that symbol in the description, it is removed from each line.
+  uncommented_lines = [
     for line in local.description_lines : 
-    trimspace(replace(line, "#", ""))
+    trimspace(trimprefix(line, "#"))
   ]
-  description = join( " ", local.updated_description_lines)
+
+  # The description can be written on multiple line.
+  # Because of that, we need to join every line into a single string for the description.
+  description = join( " ", local.uncommented_lines)
 }
 
 output "raw_lines" {
   value = local.raw_lines
 }
 
-output "first_blank_line" {
-  value = local.first_blank_line
-}
-
-output "description_lines" {
-  value = local.description_lines
+output "index" {
+  value = local.index
 }
 
 output "description" {
-  value = local.description
+  description = "The description of the Sentinel policy file."
+  value       = local.description
 }
