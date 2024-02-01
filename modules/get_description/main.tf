2a59d5f3d41d4180d1765edbe9879989281ccdaf
GitHub Action <notifications@github.com>
2024-02-01 18:31:02 +0000

    Automated Terraform Formatting

--- modules/get_description/main.tf
+++ modules/get_description/main.tf
@@ -13,15 +13,15 @@ locals {
   # Extract description line from the entire file.
   # The description should starts on the first line of the file and finish with the blank line identify in the index.
   description_lines = slice(local.raw_lines, 0, local.index)
-  
+
   # A commented line must begin with the "#" symbol.
   # Because we don't want that symbol in the description, it is removed from each line.
   uncommented_lines = [
-    for line in local.description_lines : 
+    for line in local.description_lines :
     trimspace(trimprefix(line, "#"))
   ]
 
   # The description can be written on multiple line.
   # Because of that, we need to join every line into a single string for the description.
-  description = join( " ", local.uncommented_lines)
+  description = join(" ", local.uncommented_lines)
 }
